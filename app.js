3b740eef5314842a7cee4ccf95b0f8b622d4fee1
Kristency <nandisubhajit666@gmail.com>
2019-07-11 23:17:10 +0530

    set up all CRUD routes

--- app.js
+++ app.js
@@ -5,15 +5,36 @@ const cors = require('cors');
 
 //app config
 app.use(cors());
-app.use(express.json());
-mongoose.connect(process.env.DATABASEURL || 'mongodb://localhost:27017/streamy-api', { useNewUrlParser: true });
+app.use(express.json()); // because axios sends only json , not url-encoded
+// use bodyParser or express.urlEncoded when sending a post request directly through a form.
+
+mongoose.connect(
+	process.env.DATABASEURL || 'mongodb://localhost:27017/streamy-api',
+	{ useNewUrlParser: true }
+);
 
 //requiring models
 const Stream = require('./models/stream');
 
 //setting up routes
 app.get('/streams', (req, res) => {
-	res.send('All streams');
+	Stream.find({}).then((err, foundStreams) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.json(foundStreams);
+		}
+	});
+});
+
+app.get('/streams/:id', (req, res) => {
+	Stream.findById(req.params.id).then((err, foundStream) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.json(foundStream);
+		}
+	});
 });
 
 app.post('/streams/new', (req, res) => {
@@ -22,7 +43,32 @@ app.post('/streams/new', (req, res) => {
 		if (err) {
 			console.log(err);
 		} else {
-			res.send(createdStream);
+			res.json(createdStream);
+		}
+	});
+});
+
+app.patch('/streams/:id', (req, res) => {
+	let { title, description } = req.body;
+	Stream.findByIdAndUpdate(
+		req.params.id,
+		{ title, description },
+		(err, updatedStream) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.json(updatedStream);
+			}
+		}
+	);
+});
+
+app.delete('/streams/:id', (req, res) => {
+	Stream.findByIdAndDelete(req.params.id, err => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.json(true);
 		}
 	});
 });
